c4c055895caa9ffcbed0e79df35b3f48140e90f5
Akcel77 <Akeucel@gmail.com>
2023-09-10 00:36:20 +0200

    GetAdminUrl() up

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -3,27 +3,44 @@
 namespace App\Twig;
 
 use App\Entity\Menu;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\Routing\RouterInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
 {
 
+    const ADMIN_NAMESPACE = 'App\Controller\Admin';
 
-    public function __construct(private RouterInterface $router)
+    public function __construct(private RouterInterface $router, private AdminUrlGenerator $adminUrlGenerator)
     {
 
     }
 
-    public function getFilters()
+    public function getFilters(): array
     {
         return [
            new TwigFilter('menuLink', [$this, 'menuLink']),
         ];
     }
 
-    public function menuLink(Menu $menu)
+    public function getAdminUrl(string $controller): string
+    {
+        return $this->adminUrlGenerator
+            ->setController(self::ADMIN_NAMESPACE . DIRECTORY_SEPARATOR . $controller)
+            ->generateUrl();
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('ea_index', [$this, 'getAdminUrl']),
+        ];
+    }
+
+    public function menuLink(Menu $menu): string
     {
         $article = $menu->getArticle();
         $category = $menu->getCategory();
