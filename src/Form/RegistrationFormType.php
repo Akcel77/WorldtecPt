5a6abcf311ea3a9557c11aca6f09488c9906379f
Akcel77 <Akeucel@gmail.com>
2023-10-18 14:36:00 +0200

    Register block, login, logout

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class,[
+                'label' => 'Nome de utilizador'
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'type' => PasswordType::class,
+                'options' => [
+                    'attr' =>[
+                        'type' => 'password'
+                    ]
+                ],
+                'first_options' => ['label' => 'Senha'],
+                'second_options' => ['label' => 'Confirmar a senha'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Por favor, insira uma senha',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Sua senha deve ter pelo menos {{ limit }} caracteres',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
