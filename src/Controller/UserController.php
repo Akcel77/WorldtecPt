43b00af012b63c6bbc8e4b9ab6d965693d93d889
Akcel77 <Akeucel@gmail.com>
2023-12-01 03:20:20 +0100

    User update

---
+++ src/Controller/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user/{username}', name: 'app_user')]
+    public function index(?User $user): Response
+    {
+
+        if(!$user){
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+
+}
