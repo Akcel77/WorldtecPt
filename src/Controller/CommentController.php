f154ea96d73cee0d30e6df1588616d10fdb7e9b6
Akcel77 <Akeucel@gmail.com>
2023-12-01 03:22:11 +0100

    Update usercomment

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Comment;
+use App\Entity\User;
 use App\Repository\ArticleRepository;
 use App\Repository\CommentRepository;
 use App\Repository\UserRepository;
@@ -11,9 +12,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
+
+/**
+ * @method User getUser()
+ */
 class CommentController extends AbstractController
 {
+    /**
+     * @param Request $request
+     * @param ArticleRepository $articleRepository
+     * @param EntityManagerInterface $entityManager
+     * @param CommentRepository $commentRepository
+     * @param UserRepository $userRepository
+     * @return Response
+     */
     #[Route('/ajax/comments', name: 'comment_add')]
     public function add(
         Request $request,
@@ -39,9 +53,17 @@ class CommentController extends AbstractController
             ], Response::HTTP_BAD_REQUEST);
         }
 
+        $user = $this->getUser();
+
+        if(!$user){
+            return $this->json([
+                'code' => 'USER_NOT_AUTHENTICATED_FULLY'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
         $comment = new Comment($article);
         $comment->setContent($commentDate['content']);
-        $comment->setUser($userRepository->findOneBy(['id'=> 1]));
+        $comment->setUser($user);
         $comment->setCreatedAt(new \DateTime());
 
         $entityManager->persist($comment);
